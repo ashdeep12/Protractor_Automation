cc60de549f8fa0c38cdde728b816b288d39e768d
Admin <Admin@Laptop.attlocal.net>
2019-01-19 19:57:08 -0600

    Project Initial Commit 0119

---
+++ config/config.ts
@@ -0,0 +1,45 @@
+import * as path from "path";
+import { browser, Config } from "protractor";
+import { Reporter } from "../support/reporter";
+const jsonReports = process.cwd() + "/reports/json";
+
+export const config: Config = {
+    ////sauceUser:"ashdeep", 
+   // sauceKey:"2754532e-8286-425c-a813-b00838ffc44e",
+    
+
+    seleniumAddress: "http://127.0.0.1:4444/wd/hub",
+
+    SELENIUM_PROMISE_MANAGER: false,
+
+    baseUrl: "https://www.google.com",
+
+    capabilities: {
+        browserName: "chrome",
+    },
+
+    framework: "custom",
+    frameworkPath: require.resolve("protractor-cucumber-framework"),
+
+    specs: [
+        "../../features/*.feature",
+    ],
+
+    onPrepare: () => {
+        browser.ignoreSynchronization = true;
+        browser.manage().window().maximize();
+        Reporter.createDirectory(jsonReports);
+    },
+
+    cucumberOpts: {
+        compiler: "ts:ts-node/register",
+        format: "json:./reports/json/cucumber_report.json",
+        require: ["../../typeScript/stepdefinitions/*.js", "../../typeScript/support/*.js"],
+        strict: true,
+        tags: "@CucumberScenario or @ProtractorScenario or @TypeScriptScenario or @OutlineScenario",
+    },
+
+    onComplete: () => {
+        Reporter.createHTMLReport();
+    },
+};
